d2802e33d20b3d3e35fb3ff009e7ff95f430e3ab
Hasan ÇETİNER <77547904+HasanCetiner@users.noreply.github.com>
2021-01-30 12:00:08 +0300

    Add files via upload

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Dictionary<string, int> ilPlaka = new Dictionary<string, int>();
+
+            ilPlaka.Add("Konya", 42);
+            ilPlaka.Add("Ordu", 52);
+            ilPlaka.Add("Çorum", 19);
+            ilPlaka.Add("Antalya", 07);
+            ilPlaka.Add("Karaman", 70);
+            ilPlaka.Add("Sinop", 57);
+
+            foreach (var sehirler in ilPlaka)
+            {
+                Console.WriteLine(sehirler);
+            }
+        }
+    }
+}
