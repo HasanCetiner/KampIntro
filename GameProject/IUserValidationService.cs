878b37d4bb3e84ce37cbbcf0291e45e388ad1d9f
Hasan ÇETİNER <77547904+HasanCetiner@users.noreply.github.com>
2021-02-15 23:58:17 +0300

    Add files via upload

---
+++ GameProject/IUserValidationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    interface IUserValidationService
+    {
+        bool Validate(Gamer gamer); //UserValidationManager'de if yapısında "doğru ise..." diyebilek için void'i bool'a çevirdik.
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿--Select
+--ANSII
+Select * from Customers
+Select ContactName Adi, CompanyName Sirketadi,City sehir from  Customers
+Select * from Customers where city = 'London'
+--case insensitive (büyük küçük harf duyarsız, SQL için)
+sELeCt * from Products where categoryId = 1 or categoryId = 3
+Select * from Products where categoryId = 1 and UnitPrice >=10
+-- <> 10  (10dan farklı sayıları çağır demek)
+select * from Products order by UnitPrice asc --ascenting (artan) asc yazmasan da olur
+select * from Products order by UnitPrice desc --descending(azalan) 
+Select count(*) from Products where CategoryID = 2
+select categoryID,count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName 
+from Products inner join Categories 
+on Products.CategoryID = Categories.CategoryID 
+where Products.UnitPrice >10
+
+--Data Transformation Object
+-- inner join (her iki taploda olanları getir)
+-- left join (solda olup sağda olmayanları da getir)
